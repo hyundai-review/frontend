7ad779fd65c703ae061d181898b33e03c1a7fb0c
jsj7878 <sejin8603@naver.com>
2024-11-11 19:20:58 +0900

    design: AppWrapperContainer추가 (background-color는 변경하세요)

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,33 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import GlobalStyle from './styles/globalStyle.jsx'
+import styled from 'styled-components'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <GlobalStyle />
-    <App />
-  </StrictMode>,
-)
+function Root() {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  const AppWrapper = styled.div`
+    max-width: ${({ width }) => Math.min(1440, width)}px;
+    margin: 0 auto;
+    min-height: 100dvh;
+    background-color: black;
+  `
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <StrictMode>
+      <GlobalStyle />
+      <AppWrapper width={width}>
+        <App />
+      </AppWrapper>
+    </StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<Root />)
