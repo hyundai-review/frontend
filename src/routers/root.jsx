45a9df7185f6809e121cdf2f9f8667bfb8994ada
jsj7878 <sejin8603@naver.com>
2024-11-12 10:39:50 +0900

    refactor: searchPage 반응형 수정 및 코드 리팩토링

--- src/routers/root.jsx
+++ src/routers/root.jsx
@@ -5,6 +5,7 @@ import userRouter from './userRouter'
 const loading = <div>loading</div>
 const MainPage = lazy(() => import('@/pages/Main/Index'))
 const MyPage = lazy(() => import('@/pages/MyPage/Index'))
+const SearchPage = lazy(() => import('@/pages/Search/Index'))
 
 const root = createBrowserRouter([
   {
@@ -20,6 +21,14 @@ const root = createBrowserRouter([
     element: (
       <Suspense fallback={loading}>
         <MyPage />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/search',
+    element: (
+      <Suspense fallback={loading}>
+        <SearchPage />
       </Suspense>
     ),
   },
